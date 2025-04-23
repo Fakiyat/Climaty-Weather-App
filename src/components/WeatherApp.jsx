275987327f5598953fe7b44b6d86bb3c937eba70
Fakiyat <fakiyatafaq14@gmail.com>
2025-04-24 01:24:41 +0530

    Dynamical loaction added

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherApp.css";
 import Sunny from "../assets/Images/Sunny-weather.png";
 import Snowy from "../assets/Images/Snowfalling.png";
@@ -7,97 +7,144 @@ import Cloudy from "../assets/Images/cloudy.png";
 // import Sunny from "./../assets/Images/sunny.png";
 
 function WeatherApp() {
+  const [data, setData] = useState({});
+  const [loaction, setLoaction] = useState("");
+  const api_key = "91d1cb936f8682bef4adc123701c79bf";
+
+  const handleSearchInput = (e) => {
+    setLoaction(e.target.value);
+
+    console.log(loaction);
+  };
+
+  // const weatherData = async () => {
+  //   const url = `https://api.openweathermap.org/data/2.5/weather?q=${loaction}&units=Metric&appid=${api_key}`;
+  //   const response = await fetch(url);
+  //   const searchData = response.json();
+  //   console.log(searchData);
+  //   setData(searchData);
+  // };
+  async function weatherData2() {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${loaction}&units=Metric&appid=${api_key}`
+      );
+      const searchData = await response.json();
+      console.log(searchData);
+      setData(searchData);
+    } catch (error) {
+      console.error("Error fetching data from api", error);
+    }
+  }
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      weatherData2();
+    }
+  };
   return (
-    <div class="container">
-      <div class="weather-app">
-        <button class="theme-toggle">
-          <i class="fa-solid fa-moon"></i>
+    <div className="container">
+      <div className="weather-app">
+        <button className="theme-toggle">
+          <i className="fa-solid fa-moon"></i>
         </button>
 
-        <div class="search">
-          <div class="search-top">
-            <i class="fa-solid fa-location-crosshairs"></i>
-            <div class="loaction-name">Srinagar</div>
+        <div className="search">
+          <div className="search-top">
+            <i className="fa-solid fa-location-crosshairs"></i>
+            <div className="loaction-name">{data.name}</div>
           </div>
-          <div class="search_bar">
-            <input type="text" placeholder="Search for a City" />
-            <i class="fa-solid fa-magnifying-glass"></i>
+          <div className="search_bar">
+            <input
+              type="text"
+              placeholder="Search for a City"
+              value={loaction}
+              onChange={handleSearchInput}
+              onKeyDown={handleKeyDown}
+            />
+            <i
+              className="fa-solid fa-magnifying-glass"
+              onClick={weatherData2}
+            ></i>
           </div>
         </div>
 
-        <div class="climate">
+        <div className="climate">
           <img src={Sunny} alt="sunny" />
-          <div class="weather-type">Clear</div>
-          <div class="temp">28°C</div>
-          <div class="weather-date">
+          <div className="weather-type">
+            {data.weather ? data.weather[0].main : null}
+          </div>
+          <div className="temp">
+            {data.main ? `${Math.floor(data.main.temp)}°` : null}
+          </div>
+          <div className="weather-date">
             <p>Wed, 23 April</p>
           </div>
         </div>
 
-        <div class="extra-info">
-          <div class="info-item">
-            <i class="fa-solid fa-temperature-high info-icon"></i>
-            <div class="info-title">Feels Like</div>
-            <div class="info-value">30°C</div>
+        <div className="extra-info">
+          <div className="info-item">
+            <i className="fa-solid fa-temperature-high info-icon"></i>
+            <div className="info-title">Feels Like</div>
+            <div className="info-value">23</div>
           </div>
-          <div class="info-item">
-            <i class="fa-solid fa-compress info-icon"></i>
-            <div class="info-title">Pressure</div>
-            <div class="info-value">1013 hPa</div>
+          <div className="info-item">
+            <i className="fa-solid fa-compress info-icon"></i>
+            <div className="info-title">Pressure</div>
+            <div className="info-value">1000</div>
           </div>
-          <div class="info-item">
-            <i class="fa-solid fa-sun info-icon"></i>
-            <div class="info-title">UV Index</div>
-            <div class="info-value">8</div>
-          </div>
-        </div>
-
-        <div class="weather-data">
-          <div class="humidity">
-            <div class="data-name">Humidity</div>
-            <i class="fa-solid fa-droplet"></i>
-            <div class="data">35%</div>
-          </div>
-          <div class="wind">
-            <div class="data-name">Wind</div>
-            <i class="fa-solid fa-wind"></i>
-            <div class="data">3 km/h</div>
+          <div className="info-item">
+            <i className="fa-solid fa-sun info-icon"></i>
+            <div className="info-title">UV Index</div>
+            <div className="info-value">8</div>
           </div>
         </div>
 
-        <div class="forecast">
-          <div class="forecast-title">5-Day Forecast</div>
-          <div class="forecast-items">
-            <div class="forecast-item">
-              <div class="forecast-day">Thu</div>
-              <i class="fa-solid fa-sun forecast-icon"></i>
-              <div class="forecast-temp">29°C</div>
+        <div className="weather-data">
+          <div className="humidity">
+            <div className="data-name">Humidity</div>
+            <i className="fa-solid fa-droplet"></i>
+            <div className="data">35%</div>
+          </div>
+          <div className="wind">
+            <div className="data-name">Wind</div>
+            <i className="fa-solid fa-wind"></i>
+            <div className="data">3 km/h</div>
+          </div>
+        </div>
+
+        <div className="forecast">
+          <div className="forecast-title">5-Day Forecast</div>
+          <div className="forecast-items">
+            <div className="forecast-item">
+              <div className="forecast-day">Thu</div>
+              <i className="fa-solid fa-sun forecast-icon"></i>
+              <div className="forecast-temp">29°C</div>
             </div>
-            <div class="forecast-item">
-              <div class="forecast-day">Fri</div>
-              <i class="fa-solid fa-cloud-sun forecast-icon"></i>
-              <div class="forecast-temp">27°C</div>
+            <div className="forecast-item">
+              <div className="forecast-day">Fri</div>
+              <i className="fa-solid fa-cloud-sun forecast-icon"></i>
+              <div className="forecast-temp">27°C</div>
             </div>
-            <div class="forecast-item">
-              <div class="forecast-day">Sat</div>
-              <i class="fa-solid fa-cloud forecast-icon"></i>
-              <div class="forecast-temp">25°C</div>
+            <div className="forecast-item">
+              <div className="forecast-day">Sat</div>
+              <i className="fa-solid fa-cloud forecast-icon"></i>
+              <div className="forecast-temp">25°C</div>
             </div>
-            <div class="forecast-item">
-              <div class="forecast-day">Sun</div>
-              <i class="fa-solid fa-cloud-sun-rain forecast-icon"></i>
-              <div class="forecast-temp">24°C</div>
+            <div className="forecast-item">
+              <div className="forecast-day">Sun</div>
+              <i className="fa-solid fa-cloud-sun-rain forecast-icon"></i>
+              <div className="forecast-temp">24°C</div>
             </div>
-            <div class="forecast-item">
-              <div class="forecast-day">Mon</div>
-              <i class="fa-solid fa-sun forecast-icon"></i>
-              <div class="forecast-temp">26°C</div>
+            <div className="forecast-item">
+              <div className="forecast-day">Mon</div>
+              <i className="fa-solid fa-sun forecast-icon"></i>
+              <div className="forecast-temp">26°C</div>
             </div>
           </div>
         </div>
 
-        <div class="loading">
-          <div class="spinner"></div>
+        <div className="loading">
+          <div className="spinner"></div>
         </div>
       </div>
     </div>
